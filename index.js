921a406d5aea89890777b879c58b98927c64b9bb
unknown <za0162hid@gmail.com>
2022-12-19 03:52:42 +0600

    again setup

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-const express = require("express");
-require("dotenv").config();
-const { MongoClient } = require("mongodb");
-const cors = require("cors");
-var bodyParser = require("body-parser");
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vr8ajn5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-const port = process.env.PORT || 5000;
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-app.use(bodyParser.json());
-
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.7adfu.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
-
 client.connect((err) => {
-  const bookCollection = client.db("organicdb").collection("books");
-
-  //   end the collection
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-// app.listen(port);
-app.listen(port, () => console.log(`connected database server${port}`));
